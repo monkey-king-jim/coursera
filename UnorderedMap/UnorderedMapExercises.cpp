a8d5875bfc84f03ba1ac2993a449fb7655384765
monkey-king-jim <57361690+monkey-king-jim@users.noreply.github.com>
2022-01-11 17:16:51 -0500

    Complete MP

--- UnorderedMap/UnorderedMapExercises.cpp
+++ UnorderedMap/UnorderedMapExercises.cpp
@@ -56,6 +56,9 @@ StringIntMap makeWordCounts(const StringVec& words) {
   // =================================================
   // EXERCISE 1 WORKSPACE: YOUR CODE HERE
   // =================================================
+  for (auto word: words) {
+    wordcount_map[word]++;
+  }
 
   return wordcount_map;
 }
@@ -100,8 +103,11 @@ int lookupWithFallback(const StringIntMap& wordcount_map, const std::string& key
   // =================================================
   // EXERCISE 2 WORKSPACE: YOUR CODE HERE
   // =================================================
+  if (wordcount_map.count(key) == 1) {
+    return wordcount_map.at(key);
+  }
 
-  return -1337; // Change this!
+  return fallbackVal; // Change this!
 }
 
 
@@ -214,7 +220,7 @@ int memoizedLongestPalindromeLength(LengthMemo& memo, const std::string& str, in
     // new in this case. So, we also won't store anything new in the table in
     // this case, only return what's already stored at this key in the map.
 
-    return -1337; // Hint: You need to change this!
+    return memo.at(pairKey); // Hint: You need to change this!
     // ====================================================================
 
   }
@@ -321,7 +327,8 @@ int memoizedLongestPalindromeLength(LengthMemo& memo, const std::string& str, in
   // =======================================================================
   // EXERCISE 3 - PART B - YOUR CODE HERE!
   //
-  return -1337; // Hint: You need to change this!
+  memo[pairKey] = greaterResult;
+  return greaterResult; // Hint: You need to change this!
   // =======================================================================
 }
 
